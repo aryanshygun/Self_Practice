8f1a126e6723af71e00936ac6234f1c746560ab2
aryanshygun <70066717+aryanshygun@users.noreply.github.com>
2023-12-27 20:58:30 +0330

    Add files via upload

---
+++ Lists and tuples.py
@@ -0,0 +1,38 @@
+###############################
+
+Names = ["Ryan","Majid","Leyla","Reza","Amir","Ali"]
+
+############################### entering 1 new value to the list
+
+# new_item = input(" Enter new name to add to the list: \n")
+# Names.append(new_item)
+# print(Names)
+
+############################### entering multiple values to the list
+
+# while True:
+#     new_names = input("enter new names! Once done, type 'done': \n")
+
+#     if new_names.lower() == "done":
+#         break
+#     Names.append(new_names)
+
+
+# for name in Names:
+#     print(name)
+
+############################### entering names to specific places
+
+# place = int(input(" which place? \n"))
+# new_name = input("  enter the name u wanna add: \n")
+# Names.insert(place,new_name)
+# print(Names)
+
+############################### creating lists from numbers and using ranges
+
+# numbers = [x for x in range(1,11,2)]
+# print(numbers)
+
+############################### difference with tuples ( items in tuples dont change)
+
+# dimensions = (1920,1080)
